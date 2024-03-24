9f569a5cd7de1bb1e4df7528a609bf0824d71071
Ayush Pandey <ayushpandey5@icloud.com>
2024-03-24 16:42:22 +1100

    tranfer route defined

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,6 +3,7 @@ const router = Router()
 const Account = require("../models/account")
 const User = require("../models/users")
 const authMiddleware = require("../middleware/auth")
+const mongoose = require("mongoose")
 
 
 router.get("/balance",authMiddleware, async(req,res) => {
@@ -16,6 +17,37 @@ router.get("/balance",authMiddleware, async(req,res) => {
     }
     res.status(200).json({balance: account.balance})
 
+})
+
+router.post("/transfer", authMiddleware, async(req, res) => {
+    const userId = req.userId
+    console.log(req.body)
+    if(!userId){
+        res.status(401).json({error: "Inalid account"})
+    }
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const {to, amount} = req.body
+
+    const account = await Account.findOne({userID: userId}).session(session)
+
+    if(!account || account.balance < amount){
+        session.abortTransaction();
+        return res.status(400).json({error: "Insufficient Balance"})
+    }
+
+    const toAccount = await Account.findOne({userID: to})
+    if(!toAccount){
+        session.abortTransaction()
+        return res.status(400).json({error: "Invalid account"})
+    }
+
+    await Account.updateOne({userID: userId}, {$inc: {balance: -amount}}).session(session)
+    await Account.updateOne({userId: to}, {$inc: {balance: amount}}).session(session)
+
+    await session.commitTransaction();
+    return res.status(200).json({success: "Transaction Successful"})
 })
 
 module.exports = router
