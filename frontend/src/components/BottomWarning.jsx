a36b958343b9fc7c33e9503cdb114cb21263f801
Ayush Pandey <ayushpandey5@icloud.com>
2024-03-24 19:58:26 +1100

    bottom warning text

--- frontend/src/components/BottomWarning.jsx
+++ frontend/src/components/BottomWarning.jsx
@@ -1,12 +1,14 @@
 import {Link} from "react-router-dom"
 
-export function BottomWarning({label, text, to}) {
+export function BottomWarning({label, buttonText, to}) {
   return (
     <div className="flex py-2 justify-center text-sm">
         <div>
             {label}
         </div>
-        <Link className="cursor-pointer underline pl-1 text-slate-500 "/>
+        <Link className="cursor-pointer underline pl-1 text-slate-500" to={to}>
+            {buttonText}
+        </Link>
     </div>
 
   )
